9bf686cfad30366c8a0fba606eda33965ddc7674
Philip Munksgaard <pmunksgaard@gmail.com>
2011-01-05 16:30:29 +0100

    lavet r-delen af 8-12, men mangler at skrive lidt tex

--- analyse.R
+++ analyse.R
@@ -18,6 +18,11 @@ var(logavitM) # Stikprøvevariansen af logavitM
 sd(logavitM) # Stikprøvespredningen af ligavitM
 
 # Opgave 12
+hist(logavitM, prob=T, nclass=25)
+f = function(x) dnorm(x, mean(logavitM), sd(logavitM))
+plot(f, 0,9, add=T)
 
-
-
+lnorm = function(y, mu, sigma) 1/(y*sqrt(2*pi*sigma^2)) * exp(-((log(y) - mu)^2) / (2*sigma^2))
+hist(avitM, prob=T, nclass=25)
+g = function(x) lnorm(x, mean(logavitM), sd(logavitM))
+plot(g, 0, 8000, add=T)
