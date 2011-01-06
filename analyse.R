6b8ee74c6c2f295856c20b75c38c72a353a51f5a
Philip Munksgaard <pmunksgaard@gmail.com>
2011-01-07 00:58:00 +0100

    tilføjet pdf af grafer i analyse-delen

--- analyse.R
+++ analyse.R
@@ -7,10 +7,21 @@ length(avitM) # Tæl mændene
 logavitM <- log(avitM)
 
 # Opgave 10
+pdf("graphs/analyse_1.pdf")
 hist(avitM) # Histogram for avitM
+dev.off()
+
+pdf("graphs/analyse_2.pdf")
 qqnorm(avitM) # QQ-plot for avitM
+dev.off()
+
+pdf("graphs/analyse_3.pdf")
 hist(logavitM) # Histogram for logavitM
+dev.off()
+
+pdf("graphs/analyse_4.pdf")
 qqnorm(avitM) # QQ-plot for logavitM
+dev.off()
 
 # Opgave 11
 mean(logavitM) # Gennemsnittet af logavitM
@@ -18,11 +29,15 @@ var(logavitM) # Stikprøvevariansen af logavitM
 sd(logavitM) # Stikprøvespredningen af ligavitM
 
 # Opgave 12
+pdf("graphs/analyse_5.pdf")
 hist(logavitM, prob=T, nclass=25)
 f = function(x) dnorm(x, mean(logavitM), sd(logavitM))
 plot(f, 0,9, add=T)
+dev.off()
 
 lnorm = function(y, mu, sigma) 1/(y*sqrt(2*pi*sigma^2)) * exp(-((log(y) - mu)^2) / (2*sigma^2))
+pdf("graphs/analyse_6.pdf")
 hist(avitM, prob=T, nclass=25)
 g = function(x) lnorm(x, mean(logavitM), sd(logavitM))
 plot(g, 0, 8000, add=T)
+dev.off()
